0df4f5cb7d0012bd565db2f3171d46e5500a859e
Rupesh Singh <72180684+iam-rupeshsingh@users.noreply.github.com>
2025-11-09 21:41:49 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,4 +8,4 @@ REPO="your-repo"
 # Use GitHub API with token
 curl -s -H "Authorization: token $GITHUB_TOKEN" \
 "https://api.github.com/repos/$OWNER/$REPO/collaborators" \
-| jq -r '.[] | "\(.login) - \(.permissions)"'
+| jq -r 'if type=="array" then .[] | "\(.login) - \(.permissions)" else .message end'
