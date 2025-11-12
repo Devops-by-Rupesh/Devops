9ac9563c826551d24d0e065484a645581ff5fa8d
Rupesh Singh <72180684+iam-rupeshsingh@users.noreply.github.com>
2025-11-12 13:02:33 +0530

    Update who-has-access.sh

--- who-has-access.sh
+++ who-has-access.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+# Example script: check_access.sh
+
+# GitHub username and repo name
+read -p "Enter your Gihub token: " GITHUB_TOKEN
+
+read -p "Enter your Organisation name: " OWNER
+
+read -p "Enter your Repository name: " REPO
+
+# Use GitHub API with token
+curl -s -H "Authorization: token $GITHUB_TOKEN" \
+"https://api.github.com/repos/$OWNER/$REPO/collaborators" \
+| jq -r 'if type=="array" then .[] | "\(.login) - \(.permissions)" else .message end'
